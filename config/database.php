1a175c85dbeb679153819f07f05e2cf2da92f9e1
toneybrac <toneybrac@gmail.com>
2022-09-12 04:41:19 -0700

    wip

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
 
+/*
 $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"]??null;
 $username = $url["user"]??null;
 $password = $url["pass"]??null;
 $database = substr($url["path"], 1)??null;
-
+*/
 use Illuminate\Support\Str;
 
 return [
@@ -21,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql_production'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
